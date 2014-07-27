564936d686699d8358c53cff67b285e8fe4e9c1c
qbabin <q.babin@gmail.com>
2014-07-26 23:16:21 -0700

    yala

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yala::Application.config.secret_key_base = '36561c01c2b23a1855e2862abf193bea9a663d3d9e6687572f779472c9e2ee309d2faceb53e804a32c3c34edd20e839a57f1edf732808bdacd3badccfeb033a8'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
-Rails.application.routes.draw do
+Yala::Application.routes.draw do
+get "chats/room"
+  get "sessions/new"
+  get "sessions/create"
+  get  '/chatroom' => 'chats#room', :as => :chat
+  get  '/login' => 'sessions#new', :as => :login
+  post '/login' => 'sessions#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +59,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  
+  
+  
+  
 end
